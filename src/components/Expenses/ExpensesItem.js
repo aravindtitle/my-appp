a7eb4dd1c88cd726d4a79891e6cecd56d12db27a
aravindtitle <aravindtitle@gmail.com>
2024-01-04 08:49:53 +0530

    delete button and change title button been added

--- src/components/Expenses/ExpensesItem.js
+++ src/components/Expenses/ExpensesItem.js
@@ -3,6 +3,12 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked!!!!");
+  };
+  const deleteHandler = () => {
+    console.log("Deleted!!!!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +16,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete Expense</button>
     </Card>
   );
 };
