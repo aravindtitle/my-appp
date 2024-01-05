b365275ab85e5deb6c742e4836badf25d2cb44b6
aravindtitle <aravindtitle@gmail.com>
2024-01-05 18:35:53 +0530

    submitting the forms

--- src/components/newexpenses/ExpenseForm.js
+++ src/components/newexpenses/ExpenseForm.js
@@ -1,22 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmounty, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const onTitleChnageHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const onAmountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const onDateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submithandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmounty,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submithandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={onTitleChnageHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={onAmountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={onDateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
